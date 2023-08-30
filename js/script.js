6c2b287713b64f912a19e14be51e92e4a53c1267
Jhonas Alves <jhonasma@icloud.com>
2023-08-30 01:52:48 -0300

    add first js selectors

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const apiKey = "654586f1b5d790ddc77dd6aad9ea44ce";
+
+const inputCityElement = document.querySelector("#input-city");
+const cityNameElement = document.querySelector("#city-name");
+const countryElement = document.querySelector("#country-flag");
+const temperatureElement = document.querySelector("#temp");
+const wtConditionElement = document.querySelector("#weather-condition");
+const wtIconElement = document.querySelector("#weather-icon");
+const umidityElement = document.querySelector("#umidity-percent");
+const windElement = document.querySelector("#wind-speed");
