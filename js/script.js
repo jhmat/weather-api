d8eccf362cd308895eb331ea1cf206cf99fd6360
Jhonas Alves <jhonasma@icloud.com>
2023-08-30 02:15:08 -0300

    add event listener

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const apiKey = "654586f1b5d790ddc77dd6aad9ea44ce";
 
-const inputCityElement = document.querySelector("#input-city");
+const inputCity = document.querySelector("#input-city");
+const searchBtn = document.querySelector("#search-btn");
+
 const cityNameElement = document.querySelector("#city-name");
 const countryElement = document.querySelector("#country-flag");
 const temperatureElement = document.querySelector("#temp");
@@ -8,3 +10,9 @@ const wtConditionElement = document.querySelector("#weather-condition");
 const wtIconElement = document.querySelector("#weather-icon");
 const umidityElement = document.querySelector("#umidity-percent");
 const windElement = document.querySelector("#wind-speed");
+
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const city = inputCity.value;
+});
