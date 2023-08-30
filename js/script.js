39ae22635d2b3b6cc1ef83f8b7d0573a12892381
Jhonas Alves <jhonasma@icloud.com>
2023-08-30 03:01:30 -0300

    add weather api request

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const apiKey = "654586f1b5d790ddc77dd6aad9ea44ce";
+const apiKey = "3772b8c0a8699f8a8b16ae384e24d7dd";
 
 const inputCity = document.querySelector("#input-city");
 const searchBtn = document.querySelector("#search-btn");
@@ -11,8 +11,22 @@ const wtIconElement = document.querySelector("#weather-icon");
 const umidityElement = document.querySelector("#umidity-percent");
 const windElement = document.querySelector("#wind-speed");
 
+const getWeatherData = async (city) => {
+  const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&lang=pt_br&units=metric`;
+
+  const res = await fetch(apiURL);
+  const data = await res.json();
+
+  console.log(data);
+};
+
+const showWeatherData = (city) => {
+  getWeatherData(city);
+};
+
 searchBtn.addEventListener("click", (e) => {
   e.preventDefault();
 
   const city = inputCity.value;
+  showWeatherData(city);
 });
